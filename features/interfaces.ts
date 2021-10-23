67533dad2922f43134b32dfd0734f8ccc2383581
lokideos <archmage09@gmail.com>
2021-10-23 20:08:14 +0300

    Interfaces

---
+++ features/interfaces.ts
@@ -0,0 +1,48 @@
+// Interface declaration
+interface Vehicle {
+  name: string
+  year: Date
+  broken: boolean
+  summary(): string
+}
+
+interface Reportable {
+  summary(): string
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
+}
+
+// In order to call printVehicle() function we must
+// provide an object, that meets the specification
+// of the Vehicle interface
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary())
+  console.log(`Name: ${vehicle.name}`)
+  console.log(`Year: ${vehicle.year}`)
+  console.log(`Broken? ${vehicle.broken}`)
+}
+
+printVehicle(oldCivic)
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`
+  }
+}
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary()) 
+}
+
+printSummary(oldCivic)
+printSummary(drink)
